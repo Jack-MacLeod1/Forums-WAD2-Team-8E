056da4fae5faf93daa3387e4148e3efae4e47ad9
cheesemori <2274159818@qq.com>
2026-03-02 03:04:24 +0000

    Add Category and Post models

--- forum_app/models.py
+++ forum_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -9,3 +10,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=128)
+    content = models.TextField()
+    views = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
