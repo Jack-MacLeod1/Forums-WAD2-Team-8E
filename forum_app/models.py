4e7f15bda6718a2529c7c4cb176412b35206d900
cheesemori <2274159818@qq.com>
2026-03-02 03:07:24 +0000

    Add Comment model

--- forum_app/models.py
+++ forum_app/models.py
@@ -22,3 +22,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.creator.username} on {self.post.title}"
