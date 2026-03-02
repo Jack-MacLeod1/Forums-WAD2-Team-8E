c9604e4e73a78f7224cef0ca69ff7f72b1349125
cheesemori <2274159818@qq.com>
2026-03-02 02:03:50 +0000

    Add Category model for forum_app

--- forum_app/models.py
+++ forum_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.CharField(max_length=255, blank=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
