f0d80134fc704281b7a8eb7828dbe529fad4be77
cheesemori <2274159818@qq.com>
2026-03-02 03:24:33 +0000

    Register Category, Post, Comment, and UserProfile in admin.py

--- forum_app/admin.py
+++ forum_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from forum_app.models import Category, Post, Comment, UserProfile
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(UserProfile)
