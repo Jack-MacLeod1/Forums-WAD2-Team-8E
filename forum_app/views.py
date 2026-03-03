8b25dfd2ddb4332de75587592445b431c48d685f
cheesemori <2274159818@qq.com>
2026-03-03 12:01:02 +0000

    Add user registration form, view, and template

--- forum_app/views.py
+++ forum_app/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
 from forum_app.models import Category
+from forum_app.forms import UserForm, UserProfileForm
 
 def index(request):
     category_list = Category.objects.all()
     context_dict = {'categories': category_list}
     return render(request, 'forum_app/index.html', context=context_dict)
+
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save(commit=False)
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    context_dict = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'registered': registered
+    }
+
+    return render(request, 'forum_app/register.html', context=context_dict)
