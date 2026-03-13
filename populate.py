e5025fbb4d36319c3a6f5611852d2ca407429211
Jack MacLeod <3003754m@student.gla.ac.uk>
2026-03-13 16:12:01 +0000

    Population script creates categories, posts, comments, user, and user profile

--- populate.py
+++ populate.py
@@ -5,10 +5,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 import django
 from django.template.defaultfilters import slugify
 django.setup()
-from forum_app.models import Category, Post, Comment, User
+from forum_app.models import Category, Post, Comment, User, UserProfile
+from django.contrib.auth import get_user_model
 from random import randint
 
 def populate():
+    create_sample_user_and_profile()
     cat_dict = {"UK News" : {"description": "Current news stories of the UK",
                 "views": 50,
                 "likes": 51},
@@ -29,17 +31,40 @@ def populate():
                 "views": 60,
                 "likes": 61}}
     
-
+    comment_dict = {"c1": {"content": "This is some sample text describing c1 content",
+                           "likes": 12},
+                    "c2": {"content": "This is some sample text describing c2 comment content",
+                           "likes": 13},
+                    "c3": {"content": "This is some sample text describing c3 comment content",
+                           "likes": 18},
+    }
 
     for cat in cat_dict:
         create_category(cat, cat_dict[cat]["description"], cat_dict[cat]["views"], cat_dict[cat]["likes"])
     
     for cat in cat_dict:
         for post in post_dict:
-            create_post(post, post_dict[post]["description"], post_dict[post]["views"], post_dict[post]["likes"], Category.objects.get(name=cat))
-
+            create_post(post, post_dict[post]["description"], post_dict[post]["views"], 
+                        post_dict[post]["likes"], Category.objects.get(name=cat))
     
+    for comment in comment_dict:
+        for post in Post.objects.all():
+            create_comment(post, content = comment_dict[comment]["content"],
+                            likes = comment_dict[comment]["likes"])
 
+def create_sample_user_and_profile():
+    User = get_user_model()
+    if not User.objects.filter(username = "Sample User").exists():
+        user = User.objects.create_user(username = "Sample User",
+                                        email = "example@example.com",
+                                        password = "Glasgow-Dundee-Edinburgh")
+        user.save()
+    else:
+        user = User.objects.get(username = "Sample User")
+        
+    user_profile = UserProfile.objects.get_or_create(user = user,
+                                        bio = "Sample User bio")[0]
+    user_profile.save()
     
 
 def create_category(name, desc, views, likes):
@@ -51,12 +76,22 @@ def create_category(name, desc, views, likes):
 
 def create_post(title, content, views, likes, category):
     p = Post.objects.get_or_create(category = category,
-                                   creator = User.objects.get(username = "Jack"),
+                                   creator = User.objects.get(username = "Sample User"),
                                    title = title, content = content, 
                                    views = views, likes = likes, 
                                    )[0]
     p.save()
     return p
+
+def create_comment(post, content, likes):
+    c = Comment.objects.get_or_create(post = post,
+                                      creator = User.objects.get(username = "Sample User"),
+                                      content = content,
+                                      likes = likes
+                                      )[0]
+    c.save()
+    return c
+    
 
 if __name__ == "__main__":
     print("Populating forums with dummy data")
