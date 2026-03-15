38f5b00be4d775612f8468b4a18bf6472c6eaa79
cheesemori <2274159818@qq.com>
2026-03-15 22:35:39 +0000

    Add image field to Post model for the upcoming making posts functionality

--- forum_app/models.py
+++ forum_app/models.py
@@ -25,6 +25,7 @@ class Post(models.Model):
     views = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
